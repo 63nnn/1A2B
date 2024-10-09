14751e32b29b65917a915324dbf4852baa12b990
63nnn <seanliu0729@gmail.com>
2024-10-09 15:46:04 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,17 +1,35 @@
 import random
 
-answer = random.sample(range(1, 10), 4)
-a = b = n = 0
-while a != 4:
-    a = b = n = 0
-    user = list(input("輸入四個數字："))
-    for i in user:
-        if int(user[n]) == answer[n]:
-            a += 1
-        else:
-            if int(i) in answer:
-                b += 1
-        n += 1
-    output = ",".join(user).replace(",", "")
-    print(f"{output}: {a}A{b}B")
-print("答對了！")
+
+def generate_number():
+    digits = list(range(10))
+    random.shuffle(digits)
+    return digits[:4]
+
+
+def check_guess(guess, answer):
+    A = 0
+    B = 0
+    for i in range(4):
+        if guess[i] == answer[i]:
+            A += 1
+        elif guess[i] in answer:
+            B += 1
+    return A, B
+
+
+def main():
+    answer = generate_number()
+    print("Welcome to the 1A2B guessing game!")
+    while True:
+        guess = input("Enter your guess (four digits): ")
+        guess = [int(digit) for digit in guess]
+        A, B = check_guess(guess, answer)
+        print(f"{A}A{B}B")
+        if A == 4:
+            print("Congratulations! You've guessed the right number!")
+            break
+
+
+if __name__ == "__main__":
+    main()
